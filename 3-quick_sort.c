6861a5c277c62e676e8e352aca2c8f801c79ecf7
salehgomaa60 <salehgomaazzb@gmail.com>
2024-05-04 03:08:51 -0700

    fixing quickk sort struggled with it tooo much

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,59 +1,73 @@
 #include "sort.h"
 
 /**
- * quick_sort - function that sorts an array
- * of integers in ascending order using the
- * quick sort algorithm.
- * @array: input arrray
- * @size: size of the array
- * Return: void 
+ * swap_items - Swaps two items in an array.
+ * @array: The array to modify.
+ * @l: The index of the left item.
+ * @r: The index of the right item.
  */
-void quick_sort(int *array, size_t size)
+void swap_items(int *array, size_t l, size_t r)
 {
-	_qsort(array, 0, size - 1, size);
+	int tmp;
+
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
 }
 
 /**
- * _qsort - auxiliar function for the
- * quick_sort function.
- *
- * @a: input arrray
- * @low: index for the first element
- * @high: index for the last element
- * @size: size of the array
- * Return: void
+ * quick_sort_range_lomuto - Sorts a sub array using the
+ * quick sort algorithm and Lomuto's partition scheme.
+ * @array: The array containing the sub-array.
+ * @low: The starting position of the sub-array.
+ * @high: The ending position of the sub-array.
+ * @size: The length of the array.
  */
-void _qsort(int *a, int low, int high, int size)
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
 {
-	int p, w, i;
-	int tmp;
+	size_t k, i;
+	int pivot;
 
-	if (low < high)
+	if ((low >= high) || (array == NULL))
+		return;
+	pivot = array[high];
+	k = low;
+	for (i = low; i < high; i++)
 	{
-		p = high;
-		w = low;
-		for (i = low; i < high; i++)
+		if (array[i] <= pivot)
 		{
-			if (a[i] < a[p])
+			if (k != i)
 			{
-				if (i != w)
-				{
-					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
-					print_array(a, size);
-				}
-				w++;
+				swap_items(array, k, i);
+				print_array(array, size);
 			}
+			k++;
 		}
-		if (w != p && a[w] != a[p])
-		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
-			print_array(a, size);
-		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
+	}
+	if (k != high)
+	{
+		swap_items(array, k, high);
+		print_array(array, size);
+	}
+	if (k - low > 1)
+		quick_sort_range_lomuto(array, low, k - 1, size);
+	if (high - k > 1)
+		quick_sort_range_lomuto(array, k + 1, high, size);
+}
+
+/**
+ * quick_sort - Sorts an array using the quick sort algorithm
+ * and Lomuto's partition scheme.
+ * @array: The array to sort.
+ * @size: The length of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array != NULL)
+	{
+		quick_sort_range_lomuto(array, 0, size - 1, size);
 	}
 }
